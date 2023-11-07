d3b7534a057a35db72f4d440c017f63fd42bd699
lasse1900 <lauri.kyttala@gmail.com>
2023-11-07 16:49:13 +0200

    added image field to model & actual images, designed detail view (raff)

--- food/models.py
+++ food/models.py
@@ -10,4 +10,5 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price =models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://static.vecteezy.com/system/resources/previews/003/170/825/original/isolated-food-plate-fork-and-spoon-design-free-vector.jpg")
 
